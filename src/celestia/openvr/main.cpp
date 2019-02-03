1d0dca05cfffebcf3d76f3df5d555e73c828bccd
Chris Daley <chebizarro@gmail.com>
2019-02-03 14:02:02 -0800

    added openvr lib to thirdparty

--- src/celestia/openvr/main.cpp
+++ src/celestia/openvr/main.cpp
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <string>
 #include <cstdlib>
+#include <memory>
 
 #include <openvr.h>
 
@@ -28,27 +29,132 @@
 #define APIENTRY
 #endif
 
+#include <celestia/celestiacore.h>
+
 
 class OpenVRApplication {
 
 public:
     OpenVRApplication(int argc, char *argv[]);
-    virtual ~OpenVRApplication();
+    virtual ~OpenVRApplication() = default;
 
     bool Init();
     bool InitGL();
     bool InitCompositor();
     void ShutDown();
     void RunMainLoop();
+    bool HandleInput();
+    void RenderFrame();
+    void ProcessVREvent(const vr::VREvent_t &event);
 
 private:
+
+    SDL_GLContext m_pContext;
+    SDL_Window *m_pCompanionWindow;
+    uint32_t m_nCompanionWindowWidth;
+    uint32_t m_nCompanionWindowHeight;
+
+    bool m_bVblank;
+
     vr::IVRSystem *m_pHMD;
     std::string m_strDriver;
     std::string m_strDisplay;
 
-    CelestiaCore* m_appCore{ nullptr };
+    std::unique_ptr<CelestiaCore> m_appCore;
 
 };
+
+std::string GetTrackedDeviceString( vr::TrackedDeviceIndex_t unDevice, vr::TrackedDeviceProperty prop, vr::TrackedPropertyError *peError = nullptr) {
+    auto unRequiredBufferLen = vr::VRSystem()->GetStringTrackedDeviceProperty( unDevice, prop, nullptr, 0, peError );
+    if(unRequiredBufferLen == 0)
+        return "";
+
+    char *pchBuffer = new char[unRequiredBufferLen];
+    vr::VRSystem()->GetStringTrackedDeviceProperty(unDevice, prop, pchBuffer, unRequiredBufferLen, peError);
+    std::string sResult = pchBuffer;
+    delete [] pchBuffer;
+    return sResult;
+}
+
+OpenVRApplication::OpenVRApplication(int argc, char **argv)
+        : m_pCompanionWindow(nullptr)
+        , m_nCompanionWindowWidth( 640 )
+        , m_nCompanionWindowHeight( 320 )
+        , m_pHMD(nullptr)
+        , m_bVblank( false )
+{ }
+
+void OpenVRApplication::ShutDown() {
+
+    if(m_pHMD) {
+        vr::VR_Shutdown();
+        m_pHMD = nullptr;
+    }
+
+
+    if(m_pCompanionWindow) {
+        SDL_DestroyWindow(m_pCompanionWindow);
+        m_pCompanionWindow = nullptr;
+    }
+
+    SDL_Quit();
+
+}
+
+bool OpenVRApplication::HandleInput() {
+
+    SDL_Event sdlEvent;
+    auto bRet = false;
+
+    while (SDL_PollEvent(&sdlEvent) != 0 ) {
+        if (sdlEvent.type == SDL_QUIT) {
+            bRet = true;
+        } else if (sdlEvent.type == SDL_KEYDOWN) {
+            if (sdlEvent.key.keysym.sym == SDLK_ESCAPE
+                || sdlEvent.key.keysym.sym == SDLK_q) {
+                bRet = true;
+            }
+        }
+    }
+
+    // Process SteamVR events
+    vr::VREvent_t event;
+    while(m_pHMD->PollNextEvent(&event, sizeof(event))) {
+        ProcessVREvent(event);
+    }
+
+    return bRet;
+}
+
+void OpenVRApplication::ProcessVREvent(const vr::VREvent_t &event) {
+    switch(event.eventType) {
+        case vr::VREvent_TrackedDeviceDeactivated:
+            // dprintf( "Device %u detached.\n", event.trackedDeviceIndex );
+            break;
+        case vr::VREvent_TrackedDeviceUpdated:
+            //dprintf( "Device %u updated.\n", event.trackedDeviceIndex );
+            break;
+    }
+}
+
+void OpenVRApplication::RunMainLoop() {
+
+    auto bQuit = false;
+
+    SDL_StartTextInput();
+    SDL_ShowCursor(SDL_DISABLE);
+
+    while (!bQuit) {
+        bQuit = HandleInput();
+        RenderFrame();
+    }
+
+    SDL_StopTextInput();
+}
+
+void OpenVRApplication::RenderFrame() {
+
+}
 
 bool OpenVRApplication::Init() {
 
@@ -63,9 +169,8 @@ bool OpenVRApplication::Init() {
 
     if ( eError != vr::VRInitError_None ) {
         m_pHMD = nullptr;
-        char buf[1024];
-        sprintf_s( buf, sizeof( buf ), "Unable to init VR runtime: %s", vr::VR_GetVRInitErrorAsEnglishDescription( eError ) );
-        SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_ERROR, "VR_Init Failed", buf, NULL );
+        std::string errmsg("Unable to init VR runtime: " + std::string(vr::VR_GetVRInitErrorAsEnglishDescription(eError)));
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "VR_Init Failed", errmsg.c_str(), nullptr);
         return false;
     }
 
@@ -80,16 +185,14 @@ bool OpenVRApplication::Init() {
     SDL_GL_SetAttribute( SDL_GL_MULTISAMPLEBUFFERS, 0 );
     SDL_GL_SetAttribute( SDL_GL_MULTISAMPLESAMPLES, 0 );
 
-    m_pCompanionWindow = SDL_CreateWindow( "hellovr", nWindowPosX, nWindowPosY, m_nCompanionWindowWidth, m_nCompanionWindowHeight, unWindowFlags );
-    if (m_pCompanionWindow == NULL)
-    {
+    m_pCompanionWindow = SDL_CreateWindow("celestia", nWindowPosX, nWindowPosY, m_nCompanionWindowWidth, m_nCompanionWindowHeight, unWindowFlags);
+    if (m_pCompanionWindow == nullptr) {
         printf( "%s - Window could not be created! SDL Error: %s\n", __FUNCTION__, SDL_GetError() );
         return false;
     }
 
     m_pContext = SDL_GL_CreateContext(m_pCompanionWindow);
-    if (m_pContext == NULL)
-    {
+    if (m_pContext == nullptr) {
         printf( "%s - OpenGL context could not be created! SDL Error: %s\n", __FUNCTION__, SDL_GetError() );
         return false;
     }
@@ -100,21 +203,18 @@ bool OpenVRApplication::Init() {
         printf( "%s - Error initializing GLEW! %s\n", __FUNCTION__, glewGetErrorString( nGlewError ) );
         return false;
     }
-    glGetError(); // to clear the error caused deep in GLEW
+    glGetError();
 
-    if ( SDL_GL_SetSwapInterval( m_bVblank ? 1 : 0 ) < 0 ) {
+    if (SDL_GL_SetSwapInterval( m_bVblank ? 1 : 0 ) < 0) {
         printf( "%s - Warning: Unable to set VSync! SDL Error: %s\n", __FUNCTION__, SDL_GetError() );
         return false;
     }
 
-    m_strDriver = "No Driver";
-    m_strDisplay = "No Display";
+    m_strDriver = GetTrackedDeviceString(vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String);
+    m_strDisplay = GetTrackedDeviceString(vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String);
 
-    m_strDriver = GetTrackedDeviceString( vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String );
-    m_strDisplay = GetTrackedDeviceString( vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String );
-
-    std::string strWindowTitle = "hellovr - " + m_strDriver + " " + m_strDisplay;
-    SDL_SetWindowTitle( m_pCompanionWindow, strWindowTitle.c_str() );
+    std::string strWindowTitle = "Celestia OpenVR - " + m_strDriver + " " + m_strDisplay;
+    SDL_SetWindowTitle(m_pCompanionWindow, strWindowTitle.c_str());
 
     if (!InitGL()) {
         printf("%s - Unable to initialize OpenGL!\n", __FUNCTION__);
@@ -126,13 +226,18 @@ bool OpenVRApplication::Init() {
         return false;
     }
 
-    m_appCore = new CelestiaCore();
+    m_appCore = std::make_unique<CelestiaCore>();
 
     return true;
 }
 
 bool OpenVRApplication::InitGL() {
 
+    if (!m_appCore->initRenderer()) {
+        return false;
+    }
+
+    m_appCore->tick();
 
     return true;
 }
@@ -150,7 +255,7 @@ bool OpenVRApplication::InitCompositor() {
 
 int main(int argc, char *argv[]) {
 
-    OpenVRApplication app(arc, argv);
+    OpenVRApplication app(argc, argv);
 
     if (!app.Init()) {
         app.ShutDown();
